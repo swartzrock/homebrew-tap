3f8205437649c1966628917e84c6b82cb0b9cd90
Jason Swartz <dev@bks2.com>
2022-11-01 08:57:36 -0700

    updated release, reordered blocks

--- Formula/pace.rb
+++ Formula/pace.rb
@@ -1,25 +1,25 @@
 class Pace < Formula
   desc "A visual timer for the command line"
   homepage "https://github.com/swartzrock/pace"
-  url "https://github.com/swartzrock/pace/releases/download/0.1.0/pace-macos-x64.tar.bz2"
+  url ""
   sha256 ""
   license "ISC"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/swartzrock/pace/releases/download/0.1.0/pace-macos-x64.tar.bz2"
-      sha256 "213c0ac5185fad713c5c2e2a636aa06bf2062a1aa30819c7368b9e83bf23a775"
-
-      def install
-        bin.install "pace-macos-x64" => "pace"
-      end
-    end
     if Hardware::CPU.arm?
       url "https://github.com/swartzrock/pace/releases/download/0.1.0/pace-macos-arm64.tar.bz2"
-      sha256 "da0d240a02f373e8b41e114abbbd9e040642cfa15ff2d38790ee06ade9f0b47f"
+      sha256 "f6b042d456e776620685124b15bf0bb34daf59d21179421cccf76e8a8f769680"
 
       def install
         bin.install "pace-macos-arm64" => "pace"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/swartzrock/pace/releases/download/0.1.0/pace-macos-x64.tar.bz2"
+      sha256 "1dcb2b67ba69c69f7f8733da2721e2c9632d33160567a3bdd8743a65cad7c94c"
+
+      def install
+        bin.install "pace-macos-x64" => "pace"
       end
     end
   end
