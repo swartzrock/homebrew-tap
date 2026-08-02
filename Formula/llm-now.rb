b8a95d52b01064e35d895d6d119a251f7920c02c
Jason Swartz <dev@bks2.com>
2026-08-01 23:33:25 -0700

    Update llm-now to 2.1.0

--- Formula/llm-now.rb
+++ Formula/llm-now.rb
@@ -4,30 +4,30 @@
 class LlmNow < Formula
   desc "Make one text-generation call through an available LLM provider"
   homepage "https://github.com/swartzrock/llm-now"
-  version "1.0.0"
+  version "2.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/swartzrock/llm-now/releases/download/v1.0.0/llm-now-v1.0.0-macos-arm64.zip"
-      sha256 "0e591f0777160bca3bad477859b78567351f7bcaed335ef7766cb9dc4ca0f75b"
+      url "https://github.com/swartzrock/llm-now/releases/download/v2.1.0/llm-now-v2.1.0-macos-arm64.zip"
+      sha256 "1d5b7548ff7e50505bcfb3a5ba72f1590d1b80099ccfa3d210684356a940dc37"
     end
 
     on_intel do
-      url "https://github.com/swartzrock/llm-now/releases/download/v1.0.0/llm-now-v1.0.0-macos-x64.zip"
-      sha256 "7aa7dd71acf31a0d9f88a941afce6b8e883a64b8c51e45570932c1b392dc468b"
+      url "https://github.com/swartzrock/llm-now/releases/download/v2.1.0/llm-now-v2.1.0-macos-x64.zip"
+      sha256 "b1ce059163b78122e60930709ab41448bfeb9f1502454674ebfb9d0eae7c92bc"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/swartzrock/llm-now/releases/download/v1.0.0/llm-now-v1.0.0-linux-arm64.zip"
-      sha256 "cf833e228c07efa796c87cd8e6d44a52031c332a545d31a22cb692dfd0fe381a"
+      url "https://github.com/swartzrock/llm-now/releases/download/v2.1.0/llm-now-v2.1.0-linux-arm64.zip"
+      sha256 "880bf36a47831557ddb07b2f80d4e25ab8b5ff6a03cdabb5d9964b42b95a3dd3"
     end
 
     on_intel do
-      url "https://github.com/swartzrock/llm-now/releases/download/v1.0.0/llm-now-v1.0.0-linux-x64.zip"
-      sha256 "8467896378fa7419aaf38a80ea83a5ca6e6819c8ca4af4751700fd1cbb21a929"
+      url "https://github.com/swartzrock/llm-now/releases/download/v2.1.0/llm-now-v2.1.0-linux-x64.zip"
+      sha256 "da58653c0ba19d97b24f4ea4f31f123d33440ec49569b2f359cb98a2574a8e70"
     end
   end
 
